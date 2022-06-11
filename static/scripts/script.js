6eb40f3d435532778a9dd7e875dae5bf267d738b
Sam <64541227+samhughes98@users.noreply.github.com>
2022-06-11 20:57:36 +0100

    v0.02

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,9 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //on page load print stylised title
+  var x = document.getElementById("maincontent");
+  var y = document.getElementById("maincontent1");
+
+  const introtext = "Hi! My Name Is Sam.";
+  const introtext2 = " I'm a Web Developer.";
+  let letter = 0;
+  let letter1 = 0;
+
+  function typeText() {
+    if (letter < introtext.length) {
+      x.innerHTML += introtext.charAt(letter);
+      letter++;
+      setTimeout(typeText, 40);
+    }
+    if(letter === introtext.length) {
+      if (letter1 < introtext2.length) {
+      y.innerHTML += introtext2.charAt(letter1);
+      letter1++;
+      setTimeout(typeText, 40);
+    }
+  }
+}
+  typeText();
+});
+
+var aboutmecount = 0;
+
 const el = document.getElementById('instagram');
 
 const hiddenDiv = document.getElementById('popup1');
 
 el.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv.style.display = 'show';
+  hiddenDiv.style.display = 'block';
 });
 
 el.addEventListener('mouseout', function handleMouseOut() {
@@ -14,11 +43,11 @@ const el1 = document.getElementById('facebook');
 
 const hiddenDiv1 = document.getElementById('popup2');
 
-el1.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv1.style.display = 'show';
+el1.addEventListener('mouseover', function handleMouseOver1() {
+  hiddenDiv1.style.display = 'block';
 });
 
-el1.addEventListener('mouseout', function handleMouseOut() {
+el1.addEventListener('mouseout', function handleMouseOut1() {
   hiddenDiv1.style.display = 'none';
 });
 
@@ -27,11 +56,11 @@ const el2 = document.getElementById('twitter');
 
 const hiddenDiv2 = document.getElementById('popup3');
 
-el2.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv2.style.display = 'show';
+el2.addEventListener('mouseover', function handleMouseOver2() {
+  hiddenDiv2.style.display = 'block';
 });
 
-el2.addEventListener('mouseout', function handleMouseOut() {
+el2.addEventListener('mouseout', function handleMouseOut2() {
   hiddenDiv2.style.display = 'none';
 });
 
@@ -39,11 +68,11 @@ const el3 = document.getElementById('linkedin');
 
 const hiddenDiv3 = document.getElementById('popup4');
 
-el3.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv3.style.display = 'show';
+el3.addEventListener('mouseover', function handleMouseOver3() {
+  hiddenDiv3.style.display = 'block';
 });
 
-el3.addEventListener('mouseout', function handleMouseOut() {
+el3.addEventListener('mouseout', function handleMouseOut3() {
   hiddenDiv3.style.display = 'none';
 });
 
@@ -51,11 +80,11 @@ const el4 = document.getElementById('google');
 
 const hiddenDiv4 = document.getElementById('popup5');
 
-el4.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv4.style.display = 'show';
+el4.addEventListener('mouseover', function handleMouseOver4() {
+  hiddenDiv4.style.display = 'block';
 });
 
-el4.addEventListener('mouseout', function handleMouseOut() {
+el4.addEventListener('mouseout', function handleMouseOut4() {
   hiddenDiv4.style.display = 'none';
 });
 
@@ -63,12 +92,30 @@ const el5 = document.getElementById('youtube');
 
 const hiddenDiv5 = document.getElementById('popup6');
 
-el5.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv5.style.display = 'show';
+el5.addEventListener('mouseover', function handleMouseOver5() {
+  hiddenDiv5.style.display = 'block';
 });
 
-el5.addEventListener('mouseout', function handleMouseOut() {
+el5.addEventListener('mouseout', function handleMouseOut5() {
   hiddenDiv5.style.display = 'none';
 });
 
 
+  //scroll functions
+  const scrollToAm = () => {
+    const scrollaboutme = document.getElementById('aboutmebtn');
+    scrollaboutme.addEventListener('click', () => {
+      var scrollDiv = document.getElementById("aboutme").offsetTop;
+      window.scrollTo({ top: 930, behavior: 'smooth'});
+    });
+  };
+
+
+  function testScroll(ev){
+    if(window.pageYOffset > 500){
+      var aboutmediv = document.getElementById("aboutme");
+      aboutmediv.style.display = "block";
+  }
+}
+
+  window.onscroll=testScroll
