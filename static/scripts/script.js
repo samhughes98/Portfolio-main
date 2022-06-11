7075d47d301efe61bb613337e65e21447e5a04d9
Sam <64541227+samhughes98@users.noreply.github.com>
2022-06-11 18:11:24 +0100

    commit-one

---
+++ static/scripts/script.js
@@ -0,0 +1,74 @@
+const el = document.getElementById('instagram');
+
+const hiddenDiv = document.getElementById('popup1');
+
+el.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv.style.display = 'show';
+});
+
+el.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv.style.display = 'none';
+});
+
+const el1 = document.getElementById('facebook');
+
+const hiddenDiv1 = document.getElementById('popup2');
+
+el1.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv1.style.display = 'show';
+});
+
+el1.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv1.style.display = 'none';
+});
+
+
+const el2 = document.getElementById('twitter');
+
+const hiddenDiv2 = document.getElementById('popup3');
+
+el2.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv2.style.display = 'show';
+});
+
+el2.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv2.style.display = 'none';
+});
+
+const el3 = document.getElementById('linkedin');
+
+const hiddenDiv3 = document.getElementById('popup4');
+
+el3.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv3.style.display = 'show';
+});
+
+el3.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv3.style.display = 'none';
+});
+
+const el4 = document.getElementById('google');
+
+const hiddenDiv4 = document.getElementById('popup5');
+
+el4.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv4.style.display = 'show';
+});
+
+el4.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv4.style.display = 'none';
+});
+
+const el5 = document.getElementById('youtube');
+
+const hiddenDiv5 = document.getElementById('popup6');
+
+el5.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv5.style.display = 'show';
+});
+
+el5.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv5.style.display = 'none';
+});
+
+
